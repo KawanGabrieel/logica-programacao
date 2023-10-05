d7edb7365087021c5505a5658b643bd6b3af7d06
Kawan <kawang870@gmail.com>
2023-10-05 16:42:34 -0300

    feat: colocando as informações no HTML

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-console.log("Olá,console!");
 
-document.write("Olá, documento!");
-
-window.alert("Olá,janela");
+function mediaAluno() {
 
 //Lógica de Programação
 //Problema: obter 3 notas de um aluno, calcular a média,
@@ -13,9 +10,9 @@ console.log('Inicio do programa');
 
 //declaração das variáveis
 
-const nota1 = parseInt(prompt('Digite a primeira nota'));
-const nota2 = parseInt(prompt('Digite a segunda nota'));
-const nota3 = parseInt(prompt('Digite a terceira nota'));
+const nota1 = parseInt(document.getElementById('nota1').value);
+const nota2 = parseInt(document.getElementById('nota2').value);
+const nota3 = parseInt(document.getElementById('nota3').value);
 
 //calculo da média
 const media = (nota1 + nota2 +nota3) / 3;
@@ -24,5 +21,19 @@ console.log('A nota 1 é:', nota1);
 console.log('A nota 2 é:', nota2);
 console.log('A nota 3 é:', nota3);
 
-console.log('A média do aluno é:', media);
 
+document.getElementById('resultado').innerHTML = 'A média do aluno é: '  + media + '<br>';
+
+if (media >= 7) {
+    document.getElementById('resultado').innerHTML += 'Situação: APROVADO';
+
+} else if (media >= 5) {
+    document.getElementById('resultado').innerHTML += 'Situação: EXAME';
+} else {
+    document.getElementById('resultado').innerHTML += 'Situação: REPROVADO';
+    }
+
+
+console.log('Fim do programa');
+
+}
